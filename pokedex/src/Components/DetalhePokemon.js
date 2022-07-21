521308701851f2e4decb99dcca48f4cba3a97a25
Petrick Grillo <petrick.grillo@gmail.com>
2022-07-21 11:49:14 -0300

    Detalhes quase pronto

--- pokedex/src/Components/DetalhePokemon.js
+++ pokedex/src/Components/DetalhePokemon.js
@@ -1,6 +1,7 @@
 import { useNavigate } from "react-router-dom"
-import React from "react"
+import React, { useContext } from "react"
 import styled from "styled-components"
+import GlobalContext from "./Global/GlobalContext"
 
 const Header = styled.header`
 height:10%;
@@ -10,7 +11,8 @@ justify-content:space-evenly;
 align-items:center;
 `
 const BotaoPokedex = styled.button`
-border: 0px;
+border: none;
+background:white;
 `
 const TextoHome = styled.h3`
       text-decoration: underline;
@@ -24,16 +26,38 @@ function DetalhePokemon() {
     const goToPokedex = () => {
         navigate("/MyPokemons")
     }
+    const { pokemonUnico } = useContext(GlobalContext)
 
-
+    const renderizarDetalhes = pokemonUnico && pokemonUnico.map((pokemon) => {
+        return <div>
+            <h2>
+                #{pokemon.id}
+                <br />
+                {pokemon.name.toUpperCase()}
+            </h2>
+            <div>
+                {pokemon.types.map((tipo) => {
+                    return <div > <h4>{tipo.type.name.toUpperCase()}</h4></div>
+                })}
+            </div>
+            <div>
+                {pokemon.moves.map((movimento) => {
+                    return <div> {movimento.move.name}</div>
+                })}
+            </div>
+        </div>
+    })
+    console.log(pokemonUnico)
     return (
         <div>
             <Header>
                 <BotaoPokedex onClick={goToHome}> <TextoHome>Voltar Para a home</TextoHome></BotaoPokedex>
-                <img src="https://logodownload.org/wp-content/uploads/2017/08/pokemon-logo-8.png" width={200} />
+                <BotaoPokedex onClick={goToPokedex}> <img src="https://logodownload.org/wp-content/uploads/2017/08/pokemon-logo-8.png" width={200} /></BotaoPokedex>
                 <BotaoPokedex onClick={goToHome}> <TextoHome>Excluir da Pokédex</TextoHome></BotaoPokedex>
             </Header>
-            DETALHE Pokemon
+            <div>
+                {renderizarDetalhes}
+            </div>
         </div>
     );
 }
